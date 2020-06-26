f5c10c14bb59a160f4df5001300531c09cbfc34c
Isaac Oldwood <isaac.oldwood@gmail.com>
2020-06-26 22:36:11 +0100

    Collected teamMapData

--- RLEsports/Data_Collection.py
+++ RLEsports/Data_Collection.py
@@ -117,11 +117,56 @@ def readTeamURLs(list_name):
 
     return savedURLs
 
-def collectTeamMapInfo(match_urls):
+def collectTeamMapInfo(match_urls,team_list):
     collectedData={}
 
+    for team in team_list:
+        teamData={}
+        collectedData[team]=teamData
+
+    for matchURL in match_urls:
+        JSON=collectSeriesData(matchURL)
+        total_games=JSON['data'][0]['Team1Games'] + JSON['data'][0]['Team2Games']
+
+        for i in range(1,total_games+1):
+            #Collect data
+            gameData=collectGameData(matchURL,i)
+            try:
+                mapName=gameData['data']['Map']
+                team1=gameData['data']['Team1']
+                team2=gameData['data']['Team2']
+                result=gameData['data']['Result']
+            except KeyError:
+                print(f'No game data found for match: {matchURL}, game: {i}. Moving onto next match...')
+                break
+
+            #Add to dictionary
+            if team1 in team_list:
+                if mapName not in collectedData[team1]:
+                    collectedData[team1][mapName]= {
+                        "total": 0,
+                        "wins": 0
+                    }
+                
+                if result == team1:
+                    collectedData[team1][mapName]['wins']+=1
+                
+                collectedData[team1][mapName]['total']+=1
+
+            if team2 in team_list:
+                if mapName not in collectedData[team2]:
+                    collectedData[team2][mapName]= {
+                        "total": 0,
+                        "wins": 0
+                    }
+
+                if result == team2:
+                    collectedData[team2][mapName]['wins']+=1
+                
+                collectedData[team2][mapName]['total']+=1
 
 
+    #Save collected Data
     with open(f'RLEsports/TeamMapData.json','w') as f:
         json.dump(collectedData,f,indent=2)
 
@@ -129,7 +174,7 @@ def collectTeamMapInfo(match_urls):
 
 savedURLs=readTeamURLs('bigSix')
 
-collectedData=collectTeamMapInfo(savedURLs)
+collectedData=collectTeamMapInfo(savedURLs,big_six)
 
 print(collectedData)
 

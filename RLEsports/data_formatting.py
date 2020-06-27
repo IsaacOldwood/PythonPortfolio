933c27ddf84252bb628ae3acf8ead8e3de8413ca
Isaac Oldwood <isaac.oldwood@gmail.com>
2020-06-27 15:16:27 +0100

    Formatted Data and added the resulting graph
    Graph of top6 teams's win rates

--- RLEsports/data_formatting.py
+++ RLEsports/data_formatting.py
@@ -5,12 +5,26 @@ with open(f'RLEsports/TeamMapData.json','r') as f:
     data=json.load(f)
 
 newData={}
-mapNames=["Mannfield","Champions Field","DFH Stadium","Wasteland","Utopia Coliseum","Neo Tokyo","Urban Central","Forbidden Temple","Utopia Colosseum","Aquadome"]
+mapNames=['Mannfield', 'Champions Field', 'DFH Stadium', 'Wasteland', 'Utopia Coliseum', 'Neo Tokyo', 'Urban Central', 'Forbidden Temple', 'Utopia Colosseum', 'Aquadome', 'Salty Shores', 'Starbase ARC']
+big_six=['Renault Vitality', 'Dignitas', 'mousesports', 'G2 Esports', 'NRG Esports', 'Spacestation Gaming']
 
-for team in data:
-    data[team]
-    for mapName in data[team].keys():
-        if mapName not in mapNames:
-            mapNames.append(mapName)
 
-print(mapNames)
+for mapName in mapNames:
+    newData[mapName]={}
+    for team in big_six:
+        try:
+            total=data[team][mapName]['total']
+        except KeyError:
+            continue
+
+        wins=data[team][mapName]['wins']
+        win_rate= wins / total *100
+
+        newData[mapName][team]=round(win_rate)
+
+
+
+print(newData)
+
+with open(f'RLEsports/FormattedTeamMapData.json','w') as f:
+    json.dump(newData,f,indent=2)
